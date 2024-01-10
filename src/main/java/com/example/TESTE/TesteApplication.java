9e6db655bd20dd644e24bced7dd7e9ee57e12a40
Sofia Pilz <pilzalicia@gmail.com>
2024-01-10 17:27:19 -0300

    segundo commit

--- src/main/java/com/example/TESTE/TesteApplication.java
+++ src/main/java/com/example/TESTE/TesteApplication.java
@@ -9,6 +9,7 @@ public class TesteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TesteApplication.class, args);
 
+		System.out.println("AAAAAAAAAAAAAAAA");
 	}
 
 }
