7d8ca0b44cba328fe908d7df503bf69d7a440a7b
Sofia Pilz <pilzalicia@gmail.com>
2024-01-10 17:34:32 -0300

    quarto commit

--- src/main/java/com/example/TESTE/TesteApplication.java
+++ src/main/java/com/example/TESTE/TesteApplication.java
@@ -9,7 +9,7 @@ public class TesteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TesteApplication.class, args);
 
-		System.out.println("AAAAAAAAAAAAAAAA");
+		System.out.println("RRRRRRRRRRRRRRRRRRRRRR");
 	}
 
 }
